a4957d1bb3f6ea417a2158e095cd851f4ac67401
Juhwathi Chintala <juhwathichintala@gmail.com>
2026-06-15 01:46:15 +0530

    Day 1: E-commerce recommender system (collaborative filtering)

---
+++ recommender.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+movies = pd.read_csv("data/movies.csv")
+ratings = pd.read_csv("data/ratings.csv")
+
+def get_top_movies(n=10):
+    movie_avg_rating = ratings.groupby("movieId")["rating"].mean().reset_index()
+    movie_avg_rating = movie_avg_rating.merge(movies, on="movieId")
+
+    top_movies = movie_avg_rating.sort_values("rating", ascending=False)
+
+    return top_movies[["title", "rating"]].head(n)
+user_item_matrix = ratings.pivot(
+    index="userId",
+    columns="movieId",
+    values="rating"
+)
+
+print("\nUser-Item Matrix shape:")
+print(user_item_matrix.shape)
+
+
+print("\n🔥 Top Movies:")
+print(get_top_movies(10))
